6fd3d9a7f544222a2d411aef93de7a9d3a90f410
amosgansweet <171889997+amosgansweet@users.noreply.github.com>
2024-08-25 17:24:31 +0800

    Update RecoveryCommand.py

--- RecoveryCommand.py
+++ RecoveryCommand.py
@@ -4,7 +4,7 @@ import subprocess
 import requests
 
 def send_telegram_message(bot_token, chat_id, message):
-    telegram_url = f"https://api.telegram.org/bot{token}/sendMessage"
+    telegram_url = f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage"
     telegram_payload = {
         "chat_id": chat_id,
         "text": message,
