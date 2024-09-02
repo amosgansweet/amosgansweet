4e86f257e32327cbd7a6ffb9ffe4f52c95947a9c
amosgansweet <171889997+amosgansweet@users.noreply.github.com>
2024-09-02 14:27:55 +0800

    Update check_cron.sh

--- check_cron.sh
+++ check_cron.sh
@@ -12,6 +12,6 @@ HY2="nohup /home/${USER}/hy2/hy2.sh  >/dev/null 2>&1 &"
 
 echo "检查是否运行，如果无，就启动"
 
- ps aux | grep -v grep | grep nezha-agent > /dev/null || $CRON_NEZHA
- ps aux | grep -v grep | grep s5 > /dev/null || $CRON_S5
- ps aux | grep -v grep | grep server > /dev/null || $HY2
+ ps aux | grep -v grep | grep nezha-agent > /dev/null || eval $CRON_NEZHA
+ ps aux | grep -v grep | grep s5 > /dev/null || eval $CRON_S5
+ ps aux | grep -v grep | grep server > /dev/null || eval $HY2
